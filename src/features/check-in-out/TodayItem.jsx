5a4e7fc3d80fbebae68cd376449bc1531dad9023
nour-atef04 <nourmohamedatef04@gmail.com>
2025-12-29 23:33:13 +0200

    displayed stays for current day in Dashboard

--- src/features/check-in-out/TodayItem.jsx
+++ src/features/check-in-out/TodayItem.jsx
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
+import Tag from "../../ui/Tag";
+import { Flag } from "../../ui/Flag";
+import Button from "../../ui/Button";
+import { Link } from "react-router-dom";
+import CheckoutButton from "../check-in-out/CheckoutButton"
 
 const StyledTodayItem = styled.li`
   display: grid;
@@ -18,3 +24,44 @@ const StyledTodayItem = styled.li`
 const Guest = styled.div`
   font-weight: 500;
 `;
+
+function TodayItem({ activity }) {
+  const { id, status, guests, numNights } = activity;
+
+  return (
+    <StyledTodayItem>
+      {status == "unconfirmed" && <Tag type="green">Arriving</Tag>}
+      {status == "checked-in" && <Tag type="blue">Departing</Tag>}
+      <Flag src={guests.countryFlag} alt={`Flag of ${guests.country}`} />
+      <Guest>{guests.fullName}</Guest>
+      <div>{numNights}</div>
+
+      {status == "unconfirmed" && (
+        <Button
+          size="small"
+          variation="primary"
+          as={Link}
+          to={`/checkin/${id}`}
+        >
+          Check in
+        </Button>
+      )}
+      {status == "checked-in" && <CheckoutButton bookingId={id} />}
+    </StyledTodayItem>
+  );
+}
+
+TodayItem.propTypes = {
+  activity: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    status: PropTypes.string.isRequired,
+    guests: PropTypes.shape({
+      countryFlag: PropTypes.string.isRequired,
+      country: PropTypes.string.isRequired,
+      fullName: PropTypes.string.isRequired,
+    }).isRequired,
+    numNights: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default TodayItem;
--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import { useRecentStays } from "./useRecentStays";
 import useCabins from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity"
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -17,7 +18,6 @@ const StyledDashboardLayout = styled.div`
 export default function DashboardLayout() {
   const { bookings, isLoading: isLoading1 } = useRecentBookings();
   const {
-    // stays,
     confirmedStays,
     isLoading: isLoading2,
     numDays,
@@ -36,8 +36,8 @@ export default function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Todays activity </div>
-      <DurationChart confirmedStays={confirmedStays} /> 
+      <TodayActivity />
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
